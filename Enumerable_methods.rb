f2786f5069df5fdef12c798ef02b17e96f79ad4d
Juri <ojohnnyss@yahoo.com>
2019-06-06 23:02:55 +0300

    Update Enumerable_methods.rb

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -1,1 +1,37 @@
+module Enumerable
+	def my_each
+	
+	end
+	
+	def my_each_with_index
+	
+	end
+	
+	def my_select
+	
+	end
+	
+	def my_all
+	
+	end
+	
+	def my_none
+	
+	end
+	
+	def my_count
+	
+	end
+	
+	def my_map
+	
+	end
+	
+	def my_inject
+	
+	end
+end
 
+def multiply_els
+
+end
